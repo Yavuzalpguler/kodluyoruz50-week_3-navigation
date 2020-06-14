325bce4f55d73673f61933f4dd2d1478df40c7af
Ezran Bayantemur <ezranbayantemur@gmail.com>
2020-06-14 10:45:21 +0300

    Initialize project

---
+++ src/pages/Comments.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+
+const Comments = props => {
+    return (
+        <SafeAreaView>
+            <Text>Comments</Text>
+        </SafeAreaView>
+    )
+}
+
+export { Comments }
---
+++ src/pages/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+
+const Posts = props => {
+    return (
+        <SafeAreaView>
+            <Text>Posts</Text>
+        </SafeAreaView>
+    )
+}
+
+export { Posts }
